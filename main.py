cea4e06415ffde9ed044977f963aeab77802597b
Dmytro <tschernomord@gmail.com>
2025-01-07 22:10:40 +0100

    add possibility to delete and search notes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from src.notes.models import NotesManager
 from src.constants import COMMANDS, COMMAND_LIST
 from src.notes.models import NotesManager
 from prompt_toolkit import PromptSession
@@ -17,22 +16,32 @@ def personal_assistant_app():
     session = PromptSession(completer=command_completer)
 
     while True:
-        command = session.prompt("Enter a command: ").strip().lower()
+        try:
+            command = session.prompt("Enter a command: ").strip().lower()
 
-        match command:
-            case "add-note":
-                notes_manager.add_note()
+            match command:
+                case "add-note":
+                    notes_manager.add_note()
 
-            case "show-notes":
-                notes_manager.list_notes()
+                case "show-notes":
+                    notes_manager.list_notes()
 
-            case "exit":
-                print("Exiting application. Goodbye!")
-                break
+                case "search-notes":
+                    notes_manager.search_notes()
 
-            case _:
-                print("Invalid command. Please try again.")
-                print(commands_text)
+                case "delete-note":
+                    notes_manager.delete_note()
+
+                case "exit":
+                    print("Exiting application. Goodbye!")
+                    break
+
+                case _:
+                    print("Invalid command. Please try again.")
+                    print(commands_text)
+        except KeyboardInterrupt:
+            print("Exiting application. Goodbye!")
+            break
 
 
 if __name__ == "__main__":
--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,8 @@
 COMMANDS = {
     "add-note": "This command allows you to add a new note.",
     "show-notes": "This command displays all saved notes.",
+    "search-notes": "This command allows you to search notes by title, content, or tags.",
+    "delete-note": "This command allows you to delete a note.",
     "exit": "Exit the application.",
 }
 

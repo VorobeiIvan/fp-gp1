70e0ff659cfa20ad114de0a9c27cf38c6867d648
Dmytro <tschernomord@gmail.com>
2025-01-07 18:50:50 +0100

    feature/add-auto-complete

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+
+
+COMMANDS = {
+    "add-note": "This command allows you to add a new note.",
+    "show-notes": "This command displays all saved notes.",
+    "exit": "Exit the application.",
+}
+
+COMMAND_LIST = list(COMMANDS.keys())
